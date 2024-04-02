0c593a2437c0e588bfbd9bb693503e984d92fe8a
Zackary Schreur <z.schreur@techsmith.com>
2024-04-02 14:18:43 -0400

    Get username and password from op with vault & item id

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,56 @@
 use clap::{Parser, Subcommand};
+use std::io::{self, Write};
+use std::process::Command;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 struct Opt {
+    #[arg(short, long)]
+    vault: String,
+    #[arg(short, long)]
+    id: String,
     #[command(subcommand)]
     cmd: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
     Get,
     Store,
     Erase,
 }
 
-fn get() -> std::io::Result<()> {
+fn get(vault: String, id: String) -> io::Result<()> {
     loop {
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
+        io::stdin().read_line(&mut input)?;
         if input.trim().is_empty() {
             break;
         }
     }
 
+    let username_child = Command::new("op")
+        .arg("read")
+        .arg(format!("op://{}/{}/username", vault, id))
+        .output()?;
+
+    let password_child = Command::new("op")
+        .arg("read")
+        .arg(format!("op://{}/{}/password", vault, id))
+        .output()?;
+
+    io::stdout().write_all(b"username=")?;
+    io::stdout().write_all(&username_child.stdout)?;
+    io::stdout().write_all(b"password=")?;
+    io::stdout().write_all(&password_child.stdout)?;
+
     Ok(())
 }
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), io::Error> {
     let opt = Opt::parse();
 
     match opt.cmd {
-        Commands::Get => get(),
+        Commands::Get => get(opt.vault, opt.id),
         _ => Ok(()),
     }
 }
