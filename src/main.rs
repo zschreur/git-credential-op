3515311ef53e87ca867263edf8a36a2d54ff1a2f
Zackary Schreur <z.schreur@techsmith.com>
2024-04-02 13:29:53 -0400

    Read in credential operation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+enum Operation {
+    Get,
+    Store,
+    Erase,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let op = match args.last().map(|s| s.as_str()) {
+        Some("get") => Operation::Get,
+        Some("store") => Operation::Store,
+        Some("erase") => Operation::Erase,
+        _ => panic!("Invalid operation: {}", args[1]),
+    };
+
+    match op {
+        Operation::Get => (),
+        _ => (),
+    }
 }
